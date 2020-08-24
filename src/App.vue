710c399f577706fa9aff0137457fbc4677cb41dc
jamezzz123 <chinwokwuj@gmail.com>
2020-08-24 23:40:48 +0100

    :construction: set up monopoly board

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,116 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawerRight"
+      app
+      clipped
+      right
+    >
+      <v-list dense>
+        <v-list-item @click.stop="right = !right">
+          <v-list-item-action>
+            <v-icon>mdi-exit-to-app</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Open Temporary Drawer1</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
+    <v-app-bar
+      app
+      clipped-right
+      color="blue darken-3"
+      dark
+    >
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        icon
+        large
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
+            alt="Vuetify"
+          ></v-img></v-avatar>
       </v-btn>
+      <v-toolbar-title class="ml-0 pl-4">Toolbar</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+       <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight"></v-app-bar-nav-icon>
     </v-app-bar>
-
+   
     <v-main>
-      <HelloWorld />
+      <v-container
+        class="fill-height"
+        fluid
+      >
+         <Board/>
+        <!-- </v-row> -->
+      </v-container>
     </v-main>
+     <v-navigation-drawer
+      v-model="right"
+      fixed
+      right
+      temporary
+      hide-overlay="true"
+    > <h1>History Log</h1></v-navigation-drawer>
+    
+    <v-footer
+      app
+      color="blue-grey"
+      class="white--text"
+    >
+      <span>Vuetify</span>
+      <v-spacer></v-spacer>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Board from "./components/Board"  
 
-export default {
-  name: "App",
+  export default {
+    name:"App",
 
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
+    components:{
+      Board
+    },
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      drawerRight: null,
+      right: false,
+      left: false,
+    }),
+  }
 </script>
+
+<style>
+.v-application {
+font-family: "Oswald", sans-serif !important;
+}
+  
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,15 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
-import { preset } from "vue-cli-plugin-vuetify-preset-fortnightly/preset";
+import {
+    preset
+} from "vue-cli-plugin-vuetify-preset-fortnightly/preset";
 
 Vue.use(Vuetify);
+// import  '../assets/scss/variable.scss';
 
-export default new Vuetify({ preset ,theme: { dark: true }});
+export default new Vuetify({
+    preset,
+    theme: {
+        dark: false
+    }
+});
